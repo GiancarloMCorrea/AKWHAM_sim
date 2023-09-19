26eca4755e95e58cc3d673198d27344ea46a80f5
Giancarlo M. Correa <g.moroncorrea@gmail.com>
2023-09-19 14:18:23 +0200

    to run in parallel

--- run_sim.R
+++ run_sim.R
@@ -10,6 +10,8 @@ library(dplyr)
 library(ggplot2)
 library(wham)
 library(snowfall)
+require(doSNOW)
+require(parallel)
 
 # Set working directory:
 main_dir = "~/GitHub/AKWHAM_sim"
@@ -41,10 +43,10 @@ for(k in 1:nrow(df.scenario)) {
 # Function to run sim:
 # WARNING: before running this change main_dir in sim_core.R
 # TODO: use here::here()
-run_iter <- function(sim, scen){
-  cmd <- paste("Rscript --vanilla code/sim_core.R", sim, scen)
-  system(cmd)
-}
+# run_iter <- function(sim, scen){
+#   cmd <- paste("Rscript --vanilla code/sim_core.R", sim, scen)
+#   system(cmd)
+# }
 
 # -------------------------------------------------------------
 # Run Simulation
@@ -59,10 +61,35 @@ run_iter <- function(sim, scen){
 # sfStop()
 
 # Run in parallel several simulations for all scenarios
-sfInit(parallel=TRUE, cpus=10) # modify this
-sfExportAll()
-for(sc in 1:nrow(df.scenario)){
-    sfExportAll()
-    trash <- sfLapply(1:120, function(sim) run_iter(sim, sc))
+# sfInit(parallel=TRUE, cpus=10) # modify this
+# sfExportAll()
+# for(sc in 1:nrow(df.scenario)){
+#     sfExportAll()
+#     trash <- sfLapply(1:120, function(sim) run_iter(sim, sc))
+# }
+# sfStop()
+
+
+# Run in parallel ---------------------------------------------------------
+
+# Specify scenarios and replicates to be run:
+scenj = 41:80
+simi = 1:120
+
+# Combine in DF:
+iter_df = tidyr::crossing(scenj, simi)
+nSim = nrow(iter_df)
+
+# Specify number of cores:
+nCores = 18
+cl = makeCluster(nCores)
+registerDoSNOW(cl)
+
+# Run in parallel:
+foreach(ix = 1:nSim) %dopar% {
+  source(file.path('code', 'sim_core.R'))
+  sim_core(iter_df = iter_df[ix,])
 }
-sfStop()
+
+# Stop cluster:
+stopCluster(cl)
